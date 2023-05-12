697a3a3f8cf5239b59a540373f8f4970b433945d
KangMyoungGu <kmg1123ck@naver.com>
2023-05-12 18:29:25 +0900

    test

---
+++ src/main/kotlin/com/example/naragada/TestController.kt
@@ -0,0 +1,14 @@
+package com.example.naragada
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class TestController {
+
+    @GetMapping("/")
+    fun test() : String {
+        return "hello!!"
+    }
+
+}
---
+++ src/test/kotlin/com/example/naragada/NaragadaApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.naragada
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class NaragadaApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
