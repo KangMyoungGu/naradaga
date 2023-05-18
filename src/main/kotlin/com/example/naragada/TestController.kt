fc3a45dba2acc6015d69481c625e909247ed5c8a
KangMyoungGu <kmg1123ck@naver.com>
2023-05-18 10:24:24 +0900

    dockerrun aws json 파일 푸쉬

--- src/main/kotlin/com/example/naragada/TestController.kt
+++ src/main/kotlin/com/example/naragada/TestController.kt
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class TestController {
 
-    @GetMapping("/")
+    @GetMapping("/test")
     fun test() : String {
         return "hello!!!!!!!"
     }
